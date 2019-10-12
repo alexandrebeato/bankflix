abf6cd49d43514cd23a3f766464701d45bd2bb4a
Alexandre Beato <alebeato9@gmail.com>
2019-10-12 19:44:34 -0300

    Criando entidade da conta

---
+++ server/src/Clientes.Domain/Contas/Conta.cs
@@ -0,0 +1,45 @@
+﻿using Core.Domain.Models;
+using System;
+
+namespace Clientes.Domain.Contas
+{
+    public class Conta : Entity<Conta>
+    {
+        private Conta() { }
+
+        public Guid ClienteId { get; private set; }
+        public string Numero { get; private set; }
+        public string DigitoVerificador { get; private set; }
+        public long SaldoDisponivel { get; private set; }
+        public DateTime DataHoraCriacao { get; private set; }
+
+        public void AdicionarValorSaldo(long valor) => SaldoDisponivel += valor;
+
+        public void RemoverValorSaldo(long valor)
+        {
+            if (SaldoDisponivel < valor)
+                throw new InvalidOperationException("Saldo disponível menor que o valor solicitado para remoção.");
+
+            SaldoDisponivel -= valor;
+        }
+
+        public override bool EstaConsistente()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static class Factory
+        {
+            public static Conta CriarNovaConta(Guid id, Guid clienteId, string numero, string digitoVerificador, DateTime dataHoraCriacao) =>
+                new Conta
+                {
+                    Id = id,
+                    ClienteId = clienteId,
+                    Numero = numero,
+                    DigitoVerificador = digitoVerificador,
+                    SaldoDisponivel = 0,
+                    DataHoraCriacao = dataHoraCriacao
+                };
+        }
+    }
+}
