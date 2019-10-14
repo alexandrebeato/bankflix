9e84929c5d07bfe6b5a1d7cef87681d47723b2ef
Alexandre Beato <alebeato9@gmail.com>
2019-10-14 20:27:07 -0300

    Camada de CrossCutting concluída.

---
+++ server/src/Movimentacoes.Infra.CrossCutting/IoC/BootstrapperMovimentacoes.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Movimentacoes.Commands.Depositos;
+using Movimentacoes.Commands.Movimentacoes;
+using Movimentacoes.Commands.Transferencias;
+using Movimentacoes.CommandStack.Depositos.Events;
+using Movimentacoes.CommandStack.Depositos.Handlers;
+using Movimentacoes.CommandStack.Movimentacoes.Handlers;
+using Movimentacoes.CommandStack.Transferencias.Events;
+using Movimentacoes.CommandStack.Transferencias.Handlers;
+using Movimentacoes.Domain.Clientes.Repository;
+using Movimentacoes.Domain.Contas.Repository;
+using Movimentacoes.Domain.Depositos.Repository;
+using Movimentacoes.Domain.Movimentacoes.Repository;
+using Movimentacoes.Domain.Transferencias.Repository;
+using Movimentacoes.Infra.Data.Mongo.Repository;
+
+namespace Movimentacoes.Infra.CrossCutting.IoC
+{
+    public class BootstrapperMovimentacoes
+    {
+        public static void ConfigurarServicos(IServiceCollection services)
+        {
+            services.AddScoped<IDepositoRepository, DepositoRepository>();
+            services.AddScoped<ITransferenciaRepository, TransferenciaRepository>();
+            services.AddScoped<IMovimentacaoRepository, MovimentacaoRepository>();
+            services.AddScoped<IClienteRepository, ClienteRepository>();
+            services.AddScoped<IContaRepository, ContaRepository>();
+
+            services.AddScoped<IRequestHandler<SolicitarDepositoCommand, bool>, DepositoCommandHandler>();
+            services.AddScoped<IRequestHandler<EfetuarDepositoCommand, bool>, DepositoCommandHandler>();
+            services.AddScoped<IRequestHandler<SolicitarTransferenciaCommand, bool>, TransferenciaCommandHandler>();
+            services.AddScoped<IRequestHandler<EfetuarTransferenciaCommand, bool>, TransferenciaCommandHandler>();
+            services.AddScoped<IRequestHandler<RegistrarMovimentacaoCommand, bool>, MovimentacaoCommandHandler>();
+
+            services.AddScoped<INotificationHandler<DepositoSolicitadoEvent>, DepositoEventHandler>();
+            services.AddScoped<INotificationHandler<TransferenciaSolicitadaEvent>, TransferenciaEventHandler>();
+        }
+    }
+}
