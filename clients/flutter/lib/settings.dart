c761b0bc2f0b0bdc83d4f89bb0cd5fbcd10fe590
Alexandre Beato <alebeato9@gmail.com>
2020-05-08 04:18:55 -0300

    Corrigindo a URL.

--- clients/flutter/lib/settings.dart
+++ clients/flutter/lib/settings.dart
@@ -1,7 +1,7 @@
 import 'package:bankflix/models/clientes/cliente.model.dart';
 
 class Settings {
-  static String apiUrl = "http://192.168.100.215:5002/";
+  static String apiUrl = "http://127.0.0.1:5002/";
   static Cliente cliente;
   static String token;
 }
